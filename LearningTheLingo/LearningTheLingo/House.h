7a5ebdc0641b050c5738e128fd2263140f0a4fc4
CS491 <myork@cs.ua.edu>
2014-05-07 10:22:47 -0500

    no message

---
+++ LearningTheLingo/LearningTheLingo/House.h
@@ -0,0 +1,13 @@
+//
+//  TestObject.h
+//  LearningTheLingo
+//
+//  Created by CS491 on 5/7/14.
+//  Copyright (c) 2014 MattIsTheGreatest. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestObject : NSObject
+
+@end
---
+++ LearningTheLingo/LearningTheLingo/House.m
@@ -0,0 +1,13 @@
+//
+//  TestObject.m
+//  LearningTheLingo
+//
+//  Created by CS491 on 5/7/14.
+//  Copyright (c) 2014 MattIsTheGreatest. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
+@end
